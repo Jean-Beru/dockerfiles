e6864c9df4539eed7115dd819bad75ed41da0e1f
hubert.lenoir <hubert.lenoir@sensiolabs.com>
2016-01-12 17:57:51 +0100

    Use current user in nodejs image

---
+++ nodejs/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+UID=`stat -c %u .`
+GID=`stat -c %g .`
+
+adduser -DH nodejs || true
+sed -i "s/^nodejs:x:1000:1000/nodejs:x:$UID:$GID/g" /etc/passwd
+
+su nodejs -s /bin/sh -c "$*"
