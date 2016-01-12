39991f92ced04438bf762c33500f7cea5977921a
hubert.lenoir <hubert.lenoir@sensiolabs.com>
2016-01-12 18:26:51 +0100

    Add NodeJS home folder

--- nodejs/entrypoint.sh
+++ nodejs/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 UID=`stat -c %u .`
 GID=`stat -c %g .`
 
-adduser -D nodejs || true
+adduser -D -h /home/nodejs nodejs || true
 sed -i "s/^nodejs:x:1000:1000/nodejs:x:$UID:$GID/g" /etc/passwd
 
 su nodejs -s /bin/sh -c "$*"
