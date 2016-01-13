96633742b517533bfb923e17074a953da0c0af82
hubert.lenoir <hubert.lenoir@sensiolabs.com>
2016-01-13 17:02:17 +0100

    Check existing nodejs user

--- nodejs/entrypoint.sh
+++ nodejs/entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-set -e
 
-UID=`stat -c %u .`
-GID=`stat -c %g .`
+EXISTS=`getent passwd nodejs`
 
-adduser -D -h /home/nodejs nodejs || true
-sed -i "s/^nodejs:x:1000:1000/nodejs:x:$UID:$GID/g" /etc/passwd
-chown nodejs /home/nodejs
+if [[ -z "$EXISTS" ]]; then
+  UID=`stat -c %u .`
+  GID=`stat -c %g .`
+
+  adduser -D -h /home/nodejs nodejs
+  sed -i "s/^nodejs:x:1000:1000/nodejs:x:$UID:$GID/g" /etc/passwd
+  chown nodejs /home/nodejs
+fi
 
 su nodejs -s /bin/sh -c "$*"
